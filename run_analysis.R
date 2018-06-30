9b3f1652863ae1782fb09d4348e3cd5019402611
Jess Moore <jesscobrien@gmail.com>
2018-07-01 07:55:11 +1000

    Improved documentation in run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,6 @@
 # run_analysis.R
 
-# Wk4 Programmming Assignment - Tidying data
+# Tidying activity data
 # 1 Merges the training and the test sets to create one data set.
 # 2 Extracts only the measurements on the mean and standard deviation 
 # for each measurement.
